78edf5639f8662321e58f8005fdb53c8502fd01e
Viandsilva <85799384+Viandsilva@users.noreply.github.com>
2026-06-16 21:26:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig(() => {
   return {
-    plugins: [
-      react(),
-      tailwindcss(),
-      sitemap({
-        hostname: 'https://www.senvo.in',
-      }),
-    ],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
